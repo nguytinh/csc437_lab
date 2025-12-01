c96f3ed92f19b929bfeea505c3aad22ad2467ac8
Tinhphong Nguyen <44383317+nguytinh@users.noreply.github.com>
2025-12-01 14:18:03 -0800

    fixed vite config

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -1,5 +1,16 @@
 // app/vite.config.js
+import { resolve } from 'path';
+
 export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        newuser: resolve(__dirname, 'newuser.html')
+      }
+    }
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3000",
